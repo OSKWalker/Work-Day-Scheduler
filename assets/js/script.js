7ede09e537225542147f10dbbf8e84cc1d6762bb
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-07-27 18:31:55 -0400

    attempting to add a 12hr/24hr time toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,15 @@ let timeClock = setInterval(function() {
     thisDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
     currentDayEl.html(thisDay);
 }, 1000);
+
+// Toggle between Military-Style 24hr mode and Civilian-Style 12hr (AM/PM) mode
+function switchMode(){
+    if(hourButtonEl.html("24hr")){
+        thisDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+        hourButtonEl.html("12hr");
+    }else{
+        thisDay = moment().defaultformat();
+        hourButtonEl.html("24hr");
+
+    }
+};
