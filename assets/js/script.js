5d9f686ff0649f1342a8152babd7c2d2f4317de9
OSKWalker <84692354+OSKWalker@users.noreply.github.com>
2021-07-30 20:35:08 -0400

    completed base html markup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,9 @@
-const hourButtonEl = $("#hourToggle");
 const currentDayEl = $("#currentDay");
 const timeBlocksEl = $("#timeBlocks");
 
 let thisDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 currentDayEl.html(thisDay);
 let timeClock = setInterval(function() {
-    thisDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+    thisDay = moment().format('dddd, DD MMM YY, HH:mm:ss');
     currentDayEl.html(thisDay);
 }, 1000);
-
-// Toggle between Military-Style 24hr mode and Civilian-Style 12hr (AM/PM) mode
-function switchMode(){
-    if(hourButtonEl.html("24hr")){
-        thisDay = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
-        hourButtonEl.html("12hr");
-    }else{
-        thisDay = moment().defaultformat();
-        hourButtonEl.html("24hr");
-
-    }
-};
